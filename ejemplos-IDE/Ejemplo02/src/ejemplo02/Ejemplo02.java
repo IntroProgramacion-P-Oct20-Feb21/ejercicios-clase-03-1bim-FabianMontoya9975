d04b6a93de113d09d6859ffa592095d1b5a0eb8d
Fabián Montoya <jfmontoya1@utpl.edu.ec>
2020-11-19 11:06:12 -0500

    Participaciòn en clase

--- ejemplos-IDE/Ejemplo02/src/ejemplo02/Ejemplo02.java
+++ ejemplos-IDE/Ejemplo02/src/ejemplo02/Ejemplo02.java
@@ -15,7 +15,7 @@ public class Ejemplo02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo")
+        System.out.println("Hola Mundo");
     }
     
 }
